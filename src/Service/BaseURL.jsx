404522a5c7d846386092527c500f799023c43330
Abhay <abhay@only1loan.com>
2025-03-09 14:34:45 +0530

    base url changed

--- src/Service/BaseURL.jsx
+++ src/Service/BaseURL.jsx
@@ -1,6 +1,6 @@
 export const BASE_URL = 
-// `https://api.qualoan.com/api`
+`https://api.qualoan.com/api`
 // `https://staging.qualoan.com/api`
 // `http://localhost:8080/api`
-`https://preprod-crm.api.qualoan.com/api`
+// `https://preprod-crm.api.qualoan.com/api`
 // `http://192.168.1.29:8080/api`
